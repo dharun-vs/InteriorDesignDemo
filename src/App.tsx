0eca9a22efd45bcd47e2f51b55a47b4f24fa8343
Dharun <dharun_vesa@yahoo.com>
2026-09-21 20:05:23 +0530

    Add Contact section to page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Hero } from './components/Hero'
 import { Projects } from './components/Projects'
 import { Studio } from './components/Studio'
 import { Services } from './components/Services'
+import { Contact } from './components/Contact'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Projects />
         <Studio />
         <Services />
+        <Contact />
       </main>
     </div>
   )
