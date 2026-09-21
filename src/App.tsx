842e8f8c28cdd696e2da560eac66781871c6a37c
Dharun <dharun_vesa@yahoo.com>
2026-09-21 14:46:56 +0530

    Add Studio section to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { Projects } from './components/Projects'
+import { Studio } from './components/Studio'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main id="home" aria-label="Page content">
         <Hero />
         <Projects />
+        <Studio />
       </main>
     </div>
   )
